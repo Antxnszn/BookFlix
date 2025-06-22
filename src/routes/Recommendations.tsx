39ef3d82df0e171eb4f8438477e43c71c62e3ca1
CatusElPoderoso <cactuselpoeroso@gmail.com>
2025-06-22 17:02:57 -0600

    div para el carrusel

--- src/routes/Recommendations.tsx
+++ src/routes/Recommendations.tsx
@@ -4,7 +4,8 @@ export default function MyApp() {
   return (
     <div className="bg-[#151a24] contenedor-1">
       <h1 className='text-2xl font-bold text-white'>Top Matches</h1>
-      <p className='italic'>Calculamos tus preferencias según la información proporcionada en tu perfil</p>
+      <p className='italic'>Calculamos tus preferencias según la información proporcionada en tu perfil para asegurarte la mejor experiencia :)</p>
+      <div>[Carrusel jejej]</div>
     </div>
   );
 }
