da08da106311c57b7e91d079348bc3c7d4fdbb2b
CatusElPoderoso <cactuselpoeroso@gmail.com>
2025-06-22 17:15:47 -0600

    padding 1rem -> 2rem

--- src/routes/Recommendations.tsx
+++ src/routes/Recommendations.tsx
@@ -2,10 +2,10 @@ import '../styles/style.css';
 
 export default function MyApp() {
   return (
-    <div className="bg-[#151a24] contenedor-1">
+    <div className="contenedor-1">
       <h1 className='text-2xl font-bold text-white'>Top Matches</h1>
       <p className='italic'>Calculamos tus preferencias según la información proporcionada en tu perfil para asegurarte la mejor experiencia :)</p>
-      <div>[Carrusel jejej]</div>
+      <p>[Carrusel jejej]</p>
     </div>
   );
 }
